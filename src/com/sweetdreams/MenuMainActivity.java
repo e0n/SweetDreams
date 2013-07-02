163a9aa62525716014140633e41738c77e78c386
Philip <PhStauden@aol.com>
2013-07-02 12:34:52 +0200

    Android Project created

---
+++ src/com/sweetdreams/MenuMainActivity.java
@@ -0,0 +1,22 @@
+package com.sweetdreams;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MenuMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+}
