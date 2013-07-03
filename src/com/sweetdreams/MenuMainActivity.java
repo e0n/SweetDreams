9248e5d4df3c4e3d8832cda272d6d56e9ad287b4
Philip <PhStauden@aol.com>
2013-07-03 12:41:28 +0200

    2nd activity (tutorial)

--- src/com/sweetdreams/MenuMainActivity.java
+++ src/com/sweetdreams/MenuMainActivity.java
@@ -1,10 +1,17 @@
 package com.sweetdreams;
 
-import android.os.Bundle;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MenuMainActivity extends Activity {
+	
+	 public final static String EXTRA_MESSAGE = "com.sweetdreams.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,26 @@ public class MenuMainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+//		Intent intent = new Intent(this, DisplayMessageActivity.class);
+//		String message = "Resumed!";
+//		intent.putExtra(EXTRA_MESSAGE, message);
+//		startActivity(intent);
+	}
+	
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
